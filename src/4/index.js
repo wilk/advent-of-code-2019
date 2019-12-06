a54e1cfbf986c69e4fd0802821625cfb1c8fdb52
Wilk <vincenzo.ferrari@ristoranti.it>
2019-12-06 13:31:53 +0100

    add day 4 part 2

--- src/4/index.js
+++ src/4/index.js
@@ -6,16 +6,21 @@ for (let i = start; i < end; i++) {
   const password = String(i);
   let oneDouble = false;
   let incremental = true;
-  let j = 0;
-  for (j = 0; j < password.length - 1; j++) {
-    const d1 = parseInt(password[j]);
-    const d2 = parseInt(password[j + 1]);
-    if (d1 > d2) {
+  let repeatCounter = 1;
+  for (let j = 0; j < password.length - 1; j++) {
+    const currentDigit = parseInt(password[j]);
+    const nextDigit = parseInt(password[j + 1]);
+    if (currentDigit > nextDigit) {
       incremental = false;
       break;
     }
-    if (d1 === d2) oneDouble = true;
+    if (currentDigit === nextDigit) repeatCounter++;
+    else {
+      if (repeatCounter === 2) oneDouble = true;
+      repeatCounter = 1;
+    }
   }
+  if (repeatCounter === 2) oneDouble = true;
   if (oneDouble && incremental) {
     passwordsCount++;
   }
