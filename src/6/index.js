9d400bcd3a685c1ac073ac11de9a957d56f8c6ff
Wilk <vincenzo.ferrari@ristoranti.it>
2019-12-09 16:32:04 +0100

    add day 6 part 2

--- src/6/index.js
+++ src/6/index.js
@@ -1,9 +1,112 @@
+/*
+--- Day 6: Universal Orbit Map ---
+You've landed at the Universal Orbit Map facility on Mercury. Because navigation in space often involves transferring between orbits, the orbit maps here are useful for finding efficient routes between, for example, you and Santa. You download a map of the local orbits (your puzzle input).
+
+Except for the universal Center of Mass (COM), every object in space is in orbit around exactly one other object. An orbit looks roughly like this:
+
+                  \
+                   \
+                    |
+                    |
+AAA--> o            o <--BBB
+                    |
+                    |
+                   /
+                  /
+In this diagram, the object BBB is in orbit around AAA. The path that BBB takes around AAA (drawn with lines) is only partly shown. In the map data, this orbital relationship is written AAA)BBB, which means "BBB is in orbit around AAA".
+
+Before you use your map data to plot a course, you need to make sure it wasn't corrupted during the download. To verify maps, the Universal Orbit Map facility uses orbit count checksums - the total number of direct orbits (like the one shown above) and indirect orbits.
+
+Whenever A orbits B and B orbits C, then A indirectly orbits C. This chain can be any number of objects long: if A orbits B, B orbits C, and C orbits D, then A indirectly orbits D.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+Visually, the above map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I
+In this visual representation, when two objects are connected by a line, the one on the right directly orbits the one on the left.
+
+Here, we can count the total number of orbits as follows:
+
+D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+COM orbits nothing.
+The total number of direct and indirect orbits in this example is 42.
+
+What is the total number of direct and indirect orbits in your map data?
+
+Your puzzle answer was 417916.
+
+--- Part Two ---
+Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+
+You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets you move from any object to an object orbiting or orbited by that object.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+Visually, the above map of orbits looks like this:
+
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital transfers are required:
+
+K to J
+J to E
+E to D
+D to I
+Afterward, the map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+                 \
+                  YOU
+What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
+
+Your puzzle answer was 523.
+*/
+
 const fs = require('fs');
 const path = require('path');
 const txt = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf-8');
 const orbits = txt.split('\n');
 
-const orbitMap = orbits.reduce((acc, orbit) => {
+/*const orbitMap = orbits.reduce((acc, orbit) => {
   const split = orbit.split(')');
   const objectA = split[0];
   const objectB = split[1];
@@ -15,7 +118,6 @@ const orbitMap = orbits.reduce((acc, orbit) => {
 
 const queue = [{ node: 'COM', level: 0 }];
 let total = 0;
-let level = 0;
 while (queue.length > 0) {
   const el = queue.shift();
   total += el.level;
@@ -24,34 +126,39 @@ while (queue.length > 0) {
 }
 
 console.log(total);
-
-/*
-B)G
-C)D
-COM)B
-D)I
-D)E
-E)F
-J)K
-G)H
-B)C
-E)J
-K)L
 */
 
-/*
-TOTAL: 0 + 1 + 1 + 1 + 3 + 5 + 11 + 1 + 3 + 3 + 21 + 3
+const orbitMap = orbits.reduce((acc, orbit) => {
+  const split = orbit.split(')');
+  const source = split[0];
+  const dest = split[1];
 
-B: 2
-G: 2
-C: 3
-D: 4
-COM: 0
-I: 3
-E: 20
-F: 11
-J: 21
-K: 2
-H: 3
-L: 3
-*/
+  acc[dest] = source;
+  return acc;
+}, {});
+
+const santaAncestors = [];
+let cursor = 'SAN';
+let level = 0;
+while (cursor !== 'COM') {
+  santaAncestors.push({ node: orbitMap[cursor], level });
+  cursor = orbitMap[cursor];
+  level++;
+}
+
+cursor = 'YOU';
+level = 0;
+let commonAncestor = false;
+let total = 0;
+while (!commonAncestor) {
+  const ancestor = santaAncestors.find(el => el.node === cursor);
+  if (ancestor) {
+    commonAncestor = true;
+    total = ancestor.level + level - 1;
+  }
+
+  level++;
+  cursor = orbitMap[cursor];
+}
+
+console.log(total);
